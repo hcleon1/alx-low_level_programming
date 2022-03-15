864c6ab15274b35573981d1a6020b549c7858176
root <you@example.com>
2022-03-14 17:03:46 -0700

    using function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of our program
+ * This is the main function where everything starts
+ *
+ * Return: Always return 0 (success-)
+ */
+int main(void)
+{
+
+	puts("Programming is like a multilingual puzzle");
+
+	return (0);
+}
